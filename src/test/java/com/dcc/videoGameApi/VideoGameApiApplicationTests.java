d36dbff5c3d6ec0d6b4f5ee9ce6d22638ec97161
brettjohnson22 <br.onson@outlook.com>
2022-07-06 17:57:40 -0500

    upload

---
+++ src/test/java/com/dcc/videoGameApi/VideoGameApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcc.videoGameApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoGameApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
